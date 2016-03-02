4e0ac274a8150ec3928ecbf0ac5b478f216ced70
zaruen <thomasneuraz@gmail.com>
2016-03-02 23:53:05 +0800

    Separated navigation for each section

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
 var Observable = require('FuseJS/Observable');
 
 var yo = Observable('Yo');
-var title = Observable('Title');
+var title = Observable('Home');
+
+function setTitleHome(){
+	title.value = "Home";
+}
+
+function setTitleGames(){
+	title.value = "Games";
+}
+
+function setTitlePaternityTests(){
+	title.value = "Paternity Tests";
+}
+
+function setTitleContenders(){
+	title.value = "Contenders";
+}
+
 
 module.exports = {
 	yo : yo,
-	title : title
+	title : title,
+	setTitleHome : setTitleHome,
+	setTitleGames : setTitleGames,
+	setTitlePaternityTests : setTitlePaternityTests,
+	setTitleContenders : setTitleContenders
 };
